b04870609c74b81034981042596e312de237dbab
Shota Hoshino <lga0503@gmail.com>
2014-04-08 12:07:43 +0900

    Add payment method form type

---
+++ Form/Type/WebPayType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Quartet\Payment\WebPayBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class WebPayType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('number', 'text')
+            ->add('exp_month', 'text')
+            ->add('exp_year', 'text')
+            ->add('cvc', 'text')
+            ->add('name', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'quartet_payment_webpay';
+    }
+}
