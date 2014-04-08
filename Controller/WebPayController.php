1f486b770de33456e36f294086c146cb6648f251
Shota Hoshino <lga0503@gmail.com>
2014-04-08 12:07:10 +0900

    Add WebPayController

---
+++ Controller/WebPayController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Quartet\Payment\WebPayBundle\Controller;
+
+use JMS\Payment\CoreBundle\Entity\ExtendedData;
+use JMS\Payment\CoreBundle\Entity\PaymentInstruction;
+use JMS\Payment\CoreBundle\Model\FinancialTransactionInterface;
+use JMS\Payment\CoreBundle\Model\PaymentInstructionInterface;
+use JMS\Payment\CoreBundle\Model\PaymentInterface;
+use JMS\Payment\CoreBundle\PluginController\Result;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WebPayController extends Controller
+{
+    /**
+     * @param PaymentInstructionInterface $instruction
+     *
+     * @return Result
+     */
+    protected function completePayment(PaymentInstructionInterface $instruction)
+    {
+        $controller = $this->getPluginController();
+
+        /**
+         * @var $pendingTransaction FinancialTransactionInterface
+         * @var $payment PaymentInterface
+         */
+        if (null === $pendingTransaction = $instruction->getPendingTransaction()) {
+            $payment = $controller->createPayment($instruction->getId(), $instruction->getAmount() - $instruction->getDepositedAmount());
+        } else {
+            $payment = $pendingTransaction->getPayment();
+        }
+
+        /* @var $result Result */
+        $result = $controller->approveAndDeposit($payment->getId(), $payment->getTargetAmount());
+
+        return $result;
+    }
+
+    /**
+     * @param number $amount
+     * @param string $currency
+     * @param string $method
+     * @param array  $extendedData
+     *
+     * @return PaymentInstruction
+     */
+    protected function createPaymentInstruction($amount, $currency, $method = 'quartet_payment_webpay', array $extendedData = null)
+    {
+        if (!empty($extendedData)) {
+            $extendedData = new ExtendedData();
+            foreach ($extendedData as $k => $v) {
+                $extendedData->set($k, $v);
+            }
+        }
+
+        $instruction = new PaymentInstruction($amount, $currency, $method, $extendedData);
+
+        $this->getPluginController()->createPaymentInstruction($instruction);
+
+        return $instruction;
+    }
+
+    /**
+     * @return \JMS\Payment\CoreBundle\PluginController\EntityPluginController
+     */
+    protected function getPluginController()
+    {
+        return $this->get('payment.plugin_controller');
+    }
+
+}
