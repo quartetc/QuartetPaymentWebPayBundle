ce9ee658491c9cce21c814d649c35d9e2c433bf9
Shota Hoshino <lga0503@gmail.com>
2014-08-05 10:23:32 +0900

    cosme

--- QuartetPaymentWebPayBundle.php
+++ QuartetPaymentWebPayBundle.php
@@ -3,8 +3,6 @@
 namespace Quartet\Payment\WebPayBundle;
 
 use Quartet\Payment\WebPayBundle\DependencyInjection\QuartetPaymentWebPayExtension;
-use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class QuartetPaymentWebPayBundle extends Bundle
